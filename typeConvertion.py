d055d9ba93c0360b1938247de67bd2619229fa5a
zearkiatos <caprilespe@outlook.com>
2022-07-17 22:35:06 -0400

    Types convertions

---
+++ typeConvertion.py
@@ -0,0 +1,36 @@
+print("Int convertion")
+toInt = int(2.1)
+print(toInt)
+
+toInt = int(-2.9)
+print(toInt)
+
+toInt = int("2")
+print(toInt)
+
+toInt = int(3)
+print(toInt)
+
+print("Float convertion")
+
+toFloat = float(3)
+print(toFloat)
+
+toFloat = float("4.5")
+print(toFloat)
+
+toFloat = float(4.5)
+print(toFloat)
+
+print("String convertion")
+toStr = str(2.1)
+print(toStr)
+
+toStr = str(34+27)
+print(toStr)
+
+toStr = str(104.2+34.76)
+print(toStr)
+
+toStr = "The born year is " + str(1924)
+print(toStr)
