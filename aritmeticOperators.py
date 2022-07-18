f1839e6388637e4a2801cacf3b345eb2dff71910
zearkiatos <caprilespe@outlook.com>
2022-07-17 22:11:17 -0400

    Aritmetic operators

---
+++ aritmeticOperators.py
@@ -0,0 +1,23 @@
+a = 2 + 3 + 1 + 2
+print(a)
+
+b = 2 + 3*1 + 2
+print(b)
+
+c = (2+3)*(1 + 2)
+print(c)
+
+d = (2 + 3) * (1+2)
+print(d)
+
+e = +---6
+print(e)
+
+f = -+-+6
+print(f)
+
+g = -3/2-1
+print(g)
+
+h = -3//2-1
+print(h)
