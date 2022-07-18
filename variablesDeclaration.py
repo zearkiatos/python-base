88f03be2a5c003c79d35f8a3b15df767c33c136e
zearkiatos <caprilespe@outlook.com>
2022-07-17 21:36:52 -0400

    Declaration variables, adding instruction and dynamic type

---
+++ variablesDeclaration.py
@@ -0,0 +1,12 @@
+PI = 3.14159265359
+print (PI)
+day = "Thursday"
+print(day)
+day = "Friday"
+print(day)
+print(type(day))
+day = 21
+print(day)
+print(type(day))
+figure_area = 100
+print(figure_area)
