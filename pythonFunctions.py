42c979f423f2632e50046df55e6aa067e90f979f
zearkiatos <caprilespe@outlook.com>
2022-07-18 23:02:38 -0400

    Python functions

---
+++ pythonFunctions.py
@@ -0,0 +1,63 @@
+print("Absolute value")
+print(abs(-3))
+
+print(abs(3))
+
+print(abs(0))
+
+print("Round")
+print(round(2.1))
+
+print(round(2.9))
+
+print(round(-2.9))
+
+print(round(2))
+
+print(round(2.1451,2))
+
+print(round(2.1451,3))
+
+print(round(2.1451,0))
+
+print('More functions about numbers')
+print('Min')
+print(min(1,2))
+
+print(min(12,43.24, -3))
+
+print('Max')
+print(max(1,2))
+
+print(max(12,43.24, -3))
+
+print('Pow')
+print(pow(2,3))
+print(pow(3,4))
+
+print('Strings functions')
+print('Ord')
+print(ord('a'))
+
+print(ord('A'))
+
+print('Chr')
+print(chr(97))
+
+print(chr(65))
+
+print(chr(225))
+
+print('Input and Output functions')
+print('Input')
+name = input('Please Type your name:')
+print(name)
+
+print('help')
+help(abs)
+
+
+
+
+
+
