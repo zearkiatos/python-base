ab9a6023836996c58e23e13bd3e8a107be2a8980
zearkiatos <caprilespe@outlook.com>
2022-07-27 14:22:55 -0400

    Evaluation logic expressions

--- booleans.py
+++ booleans.py
@@ -17,4 +17,11 @@ print(0.2 == 1.2 - 1.0)
 
 print(round(0.2, 1) == round(1.2-1.0, 1))
 
-
+a = 45
+b = 30
+c = 10
+print(a+c >= b)
+print('b-c<a-b: '+str(b-c<a-b))
+print('(a > b and b > c) or (b<c and a>b): '+str((a > b and b > c) or (b<c and a>b)))
+print('a*b < a*b/c: '+str(a*b < a*b/c))
+print('a > c and b: ', str(a > c and b))
