5fcb26cfcad2fd7b21399083047f50f66143b79f
zearkiatos <caprilespe@outlook.com>
2022-07-27 08:43:12 -0400

    Booleans

---
+++ booleans.py
@@ -0,0 +1,20 @@
+print(5 == (3+2))
+
+print(5 == 6)
+
+print("hola" > "hoja")
+
+
+print("Z" < "a")
+
+age = 18
+
+suffixAge = age >= 16
+
+print(type(suffixAge))
+
+print(0.2 == 1.2 - 1.0)
+
+print(round(0.2, 1) == round(1.2-1.0, 1))
+
+
