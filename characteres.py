263d0245facb01aa622ccaf3740bddeb65026665
zearkiatos <caprilespe@outlook.com>
2022-07-31 14:16:49 -0400

    Characteres and strings

---
+++ characteres.py
@@ -0,0 +1,40 @@
+a = "a\ncadena"
+print(a, "\n")
+
+b = "one\ttwo\tthree"
+print(b, "\n")
+
+c = "1\t2\t3"
+print(c, "\n")
+
+print("aa\tbb\tcc\nxx\tyy\tzz\n")
+
+print("a \"thing\" weird. \n")
+
+print("a\\b\n")
+
+print("a\
+    b\n")
+
+print("a\
+    b\n")
+
+print("A\n string that\n busy various lines\n")
+
+print("""A 
+string that
+contain various lines \n""")
+
+print(len("abc"))
+
+print(len("a"))
+
+print(len("abcd")*4)
+
+print(len("a\nb"))
+
+print(len(""))
+
+print(len(" "))
+
+print(len("   "))
