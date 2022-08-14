4fb7e617f7b94afda4becff37a07fd7afb28a97b
zearkiatos <caprilespe@outlook.com>
2022-08-14 12:15:17 -0400

    While

---
+++ oneToOneThousand.py
@@ -0,0 +1,13 @@
+account = 0
+i = 1
+sprint = 0
+
+while i <= 1000:
+    sprint += 1
+    account += 1
+    i += 1
+
+print("The result of the sum is: ", account)
+print("\n The number of sprint was: ", sprint)
+print("\n The value of i is: ", i)
+
---
+++ while.py
@@ -0,0 +1,5 @@
+i = 0
+while i < 3:
+    print(str(i)+"\n")
+    i+=1
+print('END')
