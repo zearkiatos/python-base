6f048d78f7f5e684cfd04eb68fdfc5fe71ba2046
zearkiatos <caprilespe@outlook.com>
2022-08-18 23:03:12 -0400

    Lists

---
+++ lists.py
@@ -0,0 +1,41 @@
+number = [10, 20, 30, 40]
+
+print(number)
+
+names = ["Juan", "Antonia", "Luis", "María"]
+
+print(names)
+
+a = [1, 1+1, 6//2]
+
+print(a)
+
+variable_list = ["hola", 2.5, 5, [10, 20]]
+
+print(variable_list)
+
+vocabulary = ["apple", "cheese", "dog"]
+
+numbers = [17, 123]
+
+empty_list = []
+
+print(vocabulary, numbers, empty_list)
+
+a = [1, 2, 3]
+
+len(a)
+
+a = [1, 2]
+b = [3, 4]
+c = a + b
+print(c)
+
+print(a[1])
+
+a = [1, 2, 3, 4, 5]
+
+print(a[1:3])
+
+for i in [1, 2, 3]:
+    print(i)
