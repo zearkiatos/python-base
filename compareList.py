e9bd249a1f638562686dcbf5dad1d1e0766fd99f
zearkiatos <caprilespe@outlook.com>
2022-08-20 20:32:07 -0400

    Compare and modification lists

---
+++ compareList.py
@@ -0,0 +1,35 @@
+print([1, 2, 3] == [1, 2])
+
+print([1, 2, 3] == [1, 2, 3])
+
+print([1, 2, 3] == [1, 2, 4])
+
+print([1, 10, 100] < [2, 10, 100])
+
+print([1, 10, 100] < [1, 10, 100])
+
+print([1, 10, 100] <= [1, 10, 100])
+
+a = [1, 2,3]
+
+print(a)
+
+a[1] = 10
+
+print(a)
+
+listVariable = ["a", "b", "c", "d", "e", "f"]
+
+print(listVariable)
+
+listVariable[1:3] = ["x", "y"]
+
+print(listVariable)
+
+listVariable[1:3] = []
+
+print(listVariable)
+
+del listVariable[1]
+
+print(listVariable)
