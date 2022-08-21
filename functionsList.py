e4ebd92e63c4134709974b9856dbf58fe6f19304
zearkiatos <caprilespe@outlook.com>
2022-08-20 22:02:37 -0400

    From string to list

---
+++ functionsList.py
@@ -0,0 +1,37 @@
+string = "one two three"
+
+print(string)
+
+list_from_string = string.split()
+
+print(list_from_string)
+
+string = "Hello,how,are,you"
+
+list_from_string = string.split(",")
+
+print(list_from_string)
+
+a = " ".join(['one', 'two', 'three'])
+
+print(a)
+
+a = "--".join(['one', 'two', 'three'])
+
+print(a)
+
+a = ":".join(['one', 'two', 'three'])
+
+print(a)
+
+a = list(range(1, 4))
+
+print(a)
+
+a=list("123456789027494")
+
+print(a)
+
+b = list("The White House")
+
+print(b)
