cd9f57ff96ae07590abc5760a7badf2ebbad332a
zearkiatos <caprilespe@outlook.com>
2022-08-26 20:53:35 -0400

    Running dictionary

---
+++ runningDictionary.py
@@ -0,0 +1,24 @@
+d = {}
+
+d["Juan"] = "300 235 87 45"
+
+d["Luis"] = "311 986 47 00"
+
+d["Ana"] = "320 700 62 15"
+
+d["María"] = "312 958 88 50"
+
+print(d)
+
+for element in d:
+    print(element)
+
+for key in d.keys():
+    print(key)
+
+for value in d.values():
+    print(value)
+
+for key in d.keys():
+    print(key)
+    print(d[key])
